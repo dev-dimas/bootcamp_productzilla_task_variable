6c9bb5c8bd2ee16287c889a32e638c1c87fb35b4
dev-dimas <75769207+dev-dimas@users.noreply.github.com>
2023-02-28 20:47:50 +0700

    Update answer

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // 1. Buatlah 5 variabel yang dipakai untuk menghitung 3 angka, outputkan hasilnya
-let firstVariable = 2 + 4 + 6; //12
-let secondVariable = 6 - 4 - 2; //0
-let thirdVariable = 2 * 4 * 6; //48
-let fourthVariable = 2 ** 2 % 2; //0
-let fifthVariable = 10 / 2; //5
+let firstVariable = 2 + 4 + 6;
+let secondVariable = 6 - 4 - 2;
+let thirdVariable = 2 * 4 * 6;
+let fourthVariable = 2 ** 2 % 2;
+let fifthVariable = 10 / 2;
 console.log(firstVariable);
 console.log(secondVariable);
 console.log(thirdVariable);
@@ -11,19 +11,48 @@ console.log(fourthVariable);
 console.log(fifthVariable);
 
 // 2. Buat masing masing 2 dari operator perbandingan, outputkan hasilnya
-let compare1 = 100 > 1; //true
-let compare2 = 100 < 1; //false
-let compare3 = 100 >= 1; //true
-let compare4 = 100 <= 1; //false
-let compare5 = 100 == 100; //true
-let compare6 = 100 === 100; //true
-let compare7 = 100 != '100'; //false
-let compare8 = 100 !== '100'; //true
-console.log(compare1);
-console.log(compare2);
-console.log(compare3);
-console.log(compare4);
-console.log(compare5);
-console.log(compare6);
-console.log(compare7);
-console.log(compare8);
+let firstNumber = 50;
+let secondNumber = 10;
+let thirdNumber = 2;
+let fourthNumber = '2';
+let compare;
+
+compare = firstNumber > secondNumber;
+console.log('firstNumber > secondNumber ?, ', compare);
+compare = thirdNumber > fourthNumber;
+console.log('thirdNumber > fourthNumber ?, ', compare);
+
+compare = firstNumber < secondNumber;
+console.log('firstNumber < secondNumber ?, ', compare);
+compare = thirdNumber < fourthNumber;
+console.log('thirdNumber < fourthNumber ?, ', compare);
+
+compare = firstNumber >= secondNumber;
+console.log('firstNumber >= secondNumber ?, ', compare);
+compare = thirdNumber >= fourthNumber;
+console.log('thirdNumber >= fourthNumber ?, ', compare);
+
+compare = firstNumber <= secondNumber;
+console.log('firstNumber <= secondNumber ?, ', compare);
+compare = thirdNumber <= fourthNumber;
+console.log('thirdNumber <= fourthNumber ?, ', compare);
+
+compare = firstNumber == secondNumber;
+console.log('firstNumber == secondNumber ?, ', compare);
+compare = thirdNumber == fourthNumber;
+console.log('thirdNumber == fourthNumber ?, ', compare);
+
+compare = firstNumber === secondNumber;
+console.log('firstNumber === secondNumber ?, ', compare);
+compare = thirdNumber === fourthNumber;
+console.log('thirdNumber === fourthNumber ?, ', compare);
+
+compare = firstNumber != secondNumber;
+console.log('firstNumber != secondNumber ?, ', compare);
+compare = thirdNumber != fourthNumber;
+console.log('thirdNumber != fourthNumber ?, ', compare);
+
+compare = firstNumber !== secondNumber;
+console.log('firstNumber !== secondNumber ?, ', compare);
+compare = thirdNumber !== fourthNumber;
+console.log('thirdNumber !== fourthNumber ?, ', compare);
